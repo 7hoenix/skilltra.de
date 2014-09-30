a314e5cf5180e7f9ae8900a023baec48d008aa33
Phil Phoenix <phillip.matthew.phoenix@gmail.com>
2014-09-30 13:39:17 -0600

    Fixed homepage finally and resized images on posts-index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927192017) do
+ActiveRecord::Schema.define(version: 20140930173806) do
 
   create_table "accounts", force: true do |t|
     t.string   "bio"
