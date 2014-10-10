ee995df84b4a7a54565621d827d9fb2d4fb89dc8
Phil Phoenix <phillip.matthew.phoenix@gmail.com>
2014-10-10 17:37:48 -0600

    Finally fixed email with Evan and Scott's help fk yeah

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,13 +1,9 @@
 class Bid < ActiveRecord::Base
-	after_create :email_poster_bid
+
 
 	belongs_to :post
 	has_one :user
 
-	private 
 
-		def email_poster_bid
-			BidMailer.new_bid_mail(self).deliver
-		end
 
 end
